5310947aa39370475e611ce67d0fb3895e4e140a
owlg <owlg@users.noreply.github.com>
2014-04-04 22:02:02 +0800

    Update update.sh
    Many changes and now it tells the user verbosely about the processes being executed. It can be used to pipe to other development files and other programs. The output can serve as a helpful API for other software. Added cleaning up functionality that will remove all unwanted packages and now it gets a list of all updates, rather than just one update in the older release.

--- update.sh
+++ update.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 echo 'Updates are only obtained from the certified main server, site for security purposes.'
-echo 'Getting updates package...'
-wget --user=demo --password=demouser1 www.owlg.org/taq/update/update.zip
+echo 'Creating new package folder'
+mkdir packages
+cd packages
+echo 'Getting updates list...'
+wget --user=demo --password=demouser1 -r -np -l 1 -A zip http://www.owlg.org/update/list.txt
 echo 'Updates package retrieved.'
 echo 'Expanding update package...'
-unzip update.zip
-mv -r update /var/www/html/tamain/
+unzip *.zip
+echo 'Packages expanded.'
+echo 'Cleaning up and removing all compressed packages...'
+rm *.zip
+echo 'Cleaned.'
+echo 'Moving, applying and installing all expanded packages.'
+mv -r * /var/www/html/tamain/
 echo 'done.'
