a589b34df7113792e06699dfd1f13e5d5bb21a7d
owlg <owlg@users.noreply.github.com>
2014-04-06 09:54:14 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,10 @@ echo 'Updates are only obtained from the certified main server, site for securit
 echo 'Creating new package folder'
 mkdir packages
 cd packages
-echo 'Getting updates list...'
+echo 'Getting updates list from trusted remote server...'
+echo 'Caching and downloading all updates lists...'
+wget http://taq.owlg.org/taqu/list.txt
+echo 'Downloading packages and updates in lists...'
 wget --user=demo --password=demouser1 -r -np -l 1 -A zip http://taq.owlg.org/taqu/list.txt
 echo 'Updates package retrieved.'
 echo 'Expanding update package...'
